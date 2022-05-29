dffc119c204c2e33eed3281d67e79d8f079f06b8
Tom Wilson <tom@hyper.io>
2022-05-29 17:04:46 +0000

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { router, Route } from "tinro";
+  let count = 0;
+  router.mode.hash();
+</script>
+
+<Route path="/">
+  <h1>Permaweb Counter</h1>
+  <p>Count: {count}</p>
+  <div>
+    <button on:click={() => count++}>Inc</button>
+  </div>
+  <div>
+    <a href="/about">About Page</a>
+  </div>
+</Route>
+<Route path="/about">
+  <h1>Deploy to the Permaweb</h1>
+  <div>
+    <a href="https://github.com/twilson63/deploy-permaweb-demo">github</a>
+  </div>
+  <div><a href="/">Home Page</a></div>
+</Route>
+
+<style>
+  div {
+    margin-bottom: 16px;
+  }
+</style>
